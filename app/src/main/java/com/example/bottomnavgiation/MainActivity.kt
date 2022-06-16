aa80ce9a00932ee96b20201054a000637291ec72
abdullahkhan <abdullahkhan78756@gmail.com>
2022-06-16 12:09:32 +0500

    SimpleProfile

--- app/src/main/java/com/example/bottomnavgiation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavgiation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.bottomnavgiation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.NavHostController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -16,15 +18,35 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
        private lateinit var binding: ActivityMainBinding
 
+       lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+        //For implementation of  bottom navigation bar
         val navhost=
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController=navhost.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
+
+        //For adding Drawer Nav Menu
+        toggle=ActionBarDrawerToggle(this,binding.drawerlayout,R.string.open,R.string.close)
+        binding.drawerlayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
